70acb66f7ce8ff47e505d9b39feef5803a13659e
Cole Brookson <cole.brookson@gmail.com>
2020-01-07 16:13:46 -0700

    new task - make figure with pictures on each tree tip

---
+++ Code/task5.R
@@ -0,0 +1,4 @@
+################# 5. Get representative graphics of the species groups and plop them on the tree
+### NOTE: data is from the phylogpic.com(?) website, there will be species missing 
+### NOTE: the 'how-to' is from the manual that Tash sent before 
+### NOTE: do it by order 

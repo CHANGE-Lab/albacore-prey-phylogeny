5203d2088c4d0ca0eabdf731eb55714460965dd9
zroote <zroote@ualberta.ca>
2020-04-27 15:21:38 -0600

    updated the first plot with all of the initial traits (no new ones)
    had to change notation in first line, Tash removed the captial letters.
    
    No longer need dataframe for just trophic level, I figured out how to make that work like the rest of the columns.
    
    0/1 columns wouldn't pipe properly as integers so I switched them to factors.
    
    No longer need the weird offset that we did before (where only half of the heatmap box showed up). So I fixed the offset.
    
    Removed 'benthopelagic from vert_habitat legend, as it is no longer in the dataframe
    
    I found a better way to pipe the columns onto the tree. Before we had to call the column we wanted, as well as the species column, which we did by column number. I fixed this by putting 'my_prey_traits[ , "column", drop = FALSE]". With that calling method it works perfectly, as long as the species names are the rownames.
    
    Removed "UN" category from diel migrant, refuge, and physical defense since that value is no longer in the data. Meaning the NA's are left as blanks, rather than having their own piece in the legend.
    
    I commented out the part where we annotate the column name onto the plot itself. Now that we have more data, the names end up on top of the heatmap tiles. So I'm not sure of a solution to that right now, but I'll try to think of one

--- Code/task4.R
+++ Code/task4.R
@@ -5,60 +5,62 @@
 ######## Wish List -- Fig. 7.4 from treedata book
 
 #replacing the spaces with an underscore so the prey_species in the dataset match those in the tree data
-my_prey_traits$PreySP <- gsub(" ", "_", my_prey_traits$PreySP)
+my_prey_traits$prey_sp <- gsub(" ", "_", my_prey_traits$prey_sp)
 
-rownames(my_prey_traits) <- my_prey_traits$PreySP
-#creating a separate dataframe for the trophic level data
-#for whatever reason it does not work properly when I try to 
-#create the heatmap for trophic level, even though it is the same method as I used for the other data columns
-trophic_level<- as.data.frame(my_prey_traits[ , 11])
-rownames(trophic_level) <- my_prey_traits$PreySP
-names(trophic_level) <- "Trophic Level"
+rownames(my_prey_traits) <- my_prey_traits$prey_sp
+
+sapply(my_prey_traits,class)##need to make the columns of 0/1 from integer to factor for piping to tree
+my_prey_traits[,c("diel_migrant","refuge","season_migrant","phys_defense","transparent",
+                  "col_disrupt","countershade","photophore_PA")]<-
+  lapply(my_prey_traits[,c("diel_migrant","refuge","season_migrant","phys_defense","transparent",
+                           "col_disrupt","countershade","photophore_PA")],as.factor)
+sapply(my_prey_traits,class)##there, should work now
+
 #the selection of the 4th column of data is the species, which is used to pipe the other data onto the tree
-p1_4 <- gheatmap(mycirc, my_prey_traits[, c(4,6)], offset=-0.04, width=0.10,font.size=2,
+p1_4 <- gheatmap(mycirc, my_prey_traits[,"vert_habitat",drop=FALSE], offset=0, width=0.05,font.size=2,
                colnames_angle=95, colnames_offset_y = .25, colnames = F) +
-  scale_fill_viridis_d(name = "Vertical Habitat", breaks = c("benthic", "demersal", "benthopelagic",
+  scale_fill_viridis_d(name = "Vertical Habitat", breaks = c("benthic", "demersal",
                                                              "epipelagic", "mesopelagic", "bathypelagic"),
-                       limits = c("benthic", "demersal", "benthopelagic", "epipelagic",
+                       limits = c("benthic", "demersal", "epipelagic",
                                   "mesopelagic", "bathypelagic"), na.translate = TRUE)
 p2_4 <- p1_4 + new_scale_fill()
-p3_4 <- gheatmap(p2_4, my_prey_traits[ , c(4,7)], offset=0.01, width=0.10,font.size=2,
+p3_4 <- gheatmap(p2_4, my_prey_traits[ , "horz_habitat",drop=FALSE], offset=0.05, width=0.05,font.size=2,
                colnames_angle=-85, colnames_offset_y = 4.5, colnames = F) +
   scale_fill_viridis_d(name = "Horizontal Habitat", option = "C",
                        breaks = c("reef-associated", "coastal", "continental shelf","continental slope", "oceanic"),
                        limits = c("reef-associated", "coastal", "continental shelf","continental slope", "oceanic"),
                        na.translate = TRUE)
 p4_4 <- p3_4 + new_scale_fill()
-p5_4 <- gheatmap(p4_4, my_prey_traits[ ,c(4,12)], offset=0.06, width=0.10,font.size=2,
+p5_4 <- gheatmap(p4_4, my_prey_traits[ , "body_shape",drop=FALSE], offset=0.10, width=0.05,font.size=2,
                colnames_angle=-85, colnames_offset_y = 4.5, colnames = F) +
   scale_fill_viridis_d(name = "Body Shape", option = 'magma',
-                       breaks = c("eel-like", "elongated", "fusiform ", "globiform","compressiform", "depressiform", "unique"),
-                       limits = c("eel-like", "elongated", "fusiform ", "globiform","compressiform", "depressiform", "unique"))
+                       breaks = c("eel-like", "elongated", "fusiform", "globiform","compressiform", "depressiform", "unique"),
+                       limits = c("eel-like", "elongated", "fusiform", "globiform","compressiform", "depressiform", "unique"))
 p6_4 <- p5_4 +new_scale_fill()
-p7_4 <- gheatmap(p6_4, my_prey_traits[ ,c(4,8)], offset=0.11, width=0.10,font.size=2,
+p7_4 <- gheatmap(p6_4, my_prey_traits[ ,"diel_migrant", drop=FALSE], offset=0.15, width=0.05,font.size=2,
                colnames_angle=-85, colnames_offset_y = 4.5, colnames = F) +
-  scale_fill_viridis_d(name = "Diel Migrant\nRefuge\nPhysical Defense", option = "B",
-                       breaks = c("0", "1", "UN"),
-                       limits = c("0", "1", "UN"))
-p8_4 <- gheatmap(p7_4, my_prey_traits[ ,c(4,9)], offset=0.16, width=0.10,font.size=2,
+  scale_fill_viridis_d(name = "Diel Migrant\nRefuge\nPhysical Defense", option = "B",begin = 0,end=0.5,
+                       breaks = c("0", "1"),
+                       limits = c("0", "1"))
+p8_4 <- gheatmap(p7_4, my_prey_traits[ , "refuge",drop=FALSE], offset=0.20, width=0.05,font.size=2,
                colnames_angle=-85, colnames_offset_y = 4.5, colnames = F)+
-  scale_fill_viridis_d(name = "Diel Migrant\nRefuge\nPhysical Defense", option = "B",
-                       breaks = c("0", "1", "UN"),
-                       limits = c("0", "1", "UN"))
-p9_4 <- gheatmap(p8_4, my_prey_traits[ ,c(4,10)], offset=0.21, width=0.10,font.size=2,
+  scale_fill_viridis_d(name = "Diel Migrant\nRefuge\nPhysical Defense", option = "B",begin = 0,end=0.5,
+                       breaks = c("0", "1"),
+                       limits = c("0", "1"))
+p9_4 <- gheatmap(p8_4, my_prey_traits[ ,"phys_defense",drop=FALSE], offset=0.25, width=0.05,font.size=2,
                colnames_angle=-85, colnames_offset_y = 4.5, colnames = F) +
-  scale_fill_viridis_d(name = "Diel Migrant\nRefuge\nPhysical Defense", option = "B",
-                       breaks = c("0","1","UN"),
-                       limits = c("0","1","UN"))
+  scale_fill_viridis_d(name = "Diel Migrant\nRefuge\nPhysical Defense", option = "B",begin = 0,end=0.5,
+                       breaks = c("0","1"),
+                       limits = c("0","1"))
 p10_4 <- p9_4 + new_scale_fill()
-p11_4 <- gheatmap(p10_4, trophic_level, offset=0.31, width=0.05, font.size=2,
+p11_4 <- gheatmap(p10_4, my_prey_traits[,"trophic_level",drop=FALSE], offset=0.30, width=0.05, font.size=2,
                 colnames_angle=-85, colnames_offset_y = 4.5, colnames = F) +
   scale_fill_gradientn(name = "Trophic Level", colours = c('grey90', 'purple4'), na.value = 'white') +
   theme(legend.key.size = unit(3,'mm'),
         legend.text = element_text(size = 6.5),
         legend.title = element_text(size = 6.5),
         legend.spacing = unit(0.02,'cm'),
-        legend.position = c(0.99,0.5)) +
+        legend.position = c(0.99,0.5))# +
   annotate('text', x = 1.37, y = 5.5, label = 'Trophic Level', angle = -85, size = 2)+
   annotate('text', x = 1.32, y = 5.5, label = 'Diel Migrant', angle = -85, size = 2)+
   annotate('text', x = 1.27, y = 5.5, label = 'Refuge', angle = -85, size = 2)+
@@ -171,7 +173,7 @@ p3_6 <- gheatmap(p2_6, my_prey_traits[ ,c(4,10)], offset=0.01, width=0.10,font.s
   annotate('text', x = 1.12, y = 6.7, label = 'Physical Defence', angle = -85, size = 2.8)+
   annotate('text', x = 1.07, y = 6.4, label = 'Body Shape', angle = -85, size = 2.8)
 p4_6 <- p3_6 + new_scale_fill()
-p5_6 <- gheatmap(p4_6, trophic_level, offset=0.11, width=0.05, font.size=2,
+p5_6 <- gheatmap(p4_6, my_prey_traits[,"trophic_level",drop=FALSE], offset=0.11, width=0.05, font.size=2,
                   colnames_angle=-85, colnames_offset_y = 4.5, colnames = F) +
   scale_fill_gradientn(name = "Trophic Level", colours = c('grey90', 'purple4'), na.value = 'white',
                        guide = guide_legend(order = 3))+
